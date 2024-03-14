e2338d1a51836d844d5a9afeb4d4035ad3ef2009
sisdream <sissipascale2@gmail.com>
2024-03-14 15:57:39 +0100

    feat: usato metodo config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>{{ $title }}</h1>
+    <p>@forelse($greetings as $greeting)
+        {{ $greeting}} @unless($loop->last) , @endunless
+        @empty
+        @endforelse 
+    </p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['title'=> 'Hello World']);
+   $title = 'Hello World';
+
+    $greetings = config('greetings');
+    return view('home' , compact('title','greetings'));
 });
